4f7491d26ca9954789bbd75baf040d4aef84e162
Wumpus <michaeldavelaaryoshi@gmail.com>
2024-01-19 13:55:06 +0100

    e

--- select.php
+++ select.php
@@ -11,16 +11,15 @@ $stmt->execute();
 //ophalen alle data
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($result);
 echo "<br>";
 
 //print de data in een rij
 echo "<table border=1px>";
 foreach ($result as $row) {
     echo "<tr>";
-    echo "<td>" . $row['merk'] . "</td>";
-    echo "<td>" . $row['type'] . "</td>";
-    echo "<td>" . $row['prijs'] . "</td>";
+    echo "<td>" . $row['Merk'] . "</td>";
+    echo "<td>" . $row['Type'] . "</td>";
+    echo "<td>" . $row['Prijs'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
